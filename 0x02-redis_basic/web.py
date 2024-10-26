4b0fc989d9473e94c8e7474a3224e74862af95f8
Shiraz Mohammed Elhassan Osman Elmahi <73830525+sherazme@users.noreply.github.com>
2024-10-26 03:17:37 +0200

    advance

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+''' request caching and tracking '''
+import redis
+import requests
+from functools import wraps
+from typing import Callable
+
+
+store = redis.Redis()
+''' Redis instance '''
+
+
+def data_cacher(method: Callable) -> Callable:
+    ''' Caches output of fetched data '''
+    @wraps(method)
+    def invoke(url) -> str:
+        ''' wrapper function '''
+        store.incr(f'count:{url}')
+        result = store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        store.set(f'count:{url}', 0)
+        store.setex(f'result:{url}', 10, result)
+        return result
+    return invoke
+
+
+@data_cacher
+def get_page(url: str) -> str:
+    ''' Returns URL content after caching request's response and tracking the request '''
+    return requests.get(url).text
