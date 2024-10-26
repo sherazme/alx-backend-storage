9aebfc51663259066c22e143b87f011256c3b45f
Shiraz Mohammed Elhassan Osman Elmahi <73830525+sherazme@users.noreply.github.com>
2024-10-26 04:38:47 +0200

    test

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,27 +1,45 @@
 #!/usr/bin/env python3
-''' Task 15's module '''
+""" MongoDB Operations with Python using pymongo """
 from pymongo import MongoClient
-from collections import Counter
 
+if __name__ == "__main__":
+    """ Provides some stats about Nginx logs stored in MongoDB """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
 
-''' Connect to MongoDB '''
-client = MongoClient("mongodb://localhost:27017/")
-db = client['logs']
-collection = db['nginx']
+    n_logs = nginx_collection.count_documents({})
+    print(f'{n_logs} logs')
 
-''' Query all logs '''
-logs = collection.find()
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print('Methods:')
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print(f'\tmethod {method}: {count}')
 
-''' Extract IPs from logs '''
-ips = [log['ip'] for log in logs if 'ip' in log]
+    status_check = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
 
-''' Count occurrences of each IP '''
-ip_counter = Counter(ips)
+    print(f'{status_check} status check')
 
-''' Get the top 10 most common IPs '''
-top_ips = ip_counter.most_common(10)
+    top_ips = nginx_collection.aggregate([
+        {"$group":
+            {
+                "_id": "$ip",
+                "count": {"$sum": 1}
+            }
+         },
+        {"$sort": {"count": -1}},
+        {"$limit": 10},
+        {"$project": {
+            "_id": 0,
+            "ip": "$_id",
+            "count": 1
+        }}
+    ])
 
-''' Print the results '''
-print("Top 10 IPs:")
-for ip, count in top_ips:
-    print(f"{ip}: {count}")
+    print("IPs:")
+    for top_ip in top_ips:
+        ip = top_ip.get("ip")
+        count = top_ip.get("count")
+        print(f'\t{ip}: {count}')
