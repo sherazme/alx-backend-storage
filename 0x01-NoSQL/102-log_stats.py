3928d1db7d7448e9e63cf3c8fec000f759af3ece
Shiraz Mohammed Elhassan Osman Elmahi <73830525+sherazme@users.noreply.github.com>
2024-10-26 04:25:30 +0200

    nosql

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,54 +1,25 @@
-#!/usr/bin/env python3
-'''Task 15's module.
-'''
 from pymongo import MongoClient
+from collections import Counter
 
 
-def print_nginx_request_logs(nginx_collection):
-    '''Prints stats about Nginx request logs.
-    '''
-    print('{} logs'.format(nginx_collection.count_documents({})))
-    print('Methods:')
-    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-    for method in methods:
-        req_count = len(list(nginx_collection.find({'method': method})))
-        print('\tmethod {}: {}'.format(method, req_count))
-    status_checks_count = len(list(
-        nginx_collection.find({'method': 'GET', 'path': '/status'})
-    ))
-    print('{} status check'.format(status_checks_count))
+''' Connect to MongoDB '''
+client = MongoClient("mongodb://localhost:27017/")
+db = client['logs']
+collection = db['nginx']
 
+''' Query all logs '''
+logs = collection.find()
 
-def print_top_ips(server_collection):
-    '''Prints statistics about the top 10 HTTP IPs in a collection.
-    '''
-    print('IPs:')
-    request_logs = server_collection.aggregate(
-        [
-            {
-                '$group': {'_id': "$ip", 'totalRequests': {'$sum': 1}}
-            },
-            {
-                '$sort': {'totalRequests': -1}
-            },
-            {
-                '$limit': 10
-            },
-        ]
-    )
-    for request_log in request_logs:
-        ip = request_log['_id']
-        ip_requests_count = request_log['totalRequests']
-        print('\t{}: {}'.format(ip, ip_requests_count))
+''' Extract IPs from logs '''
+ips = [log['ip'] for log in logs if 'ip' in log]
 
+''' Count occurrences of each IP '''
+ip_counter = Counter(ips)
 
-def run():
-    '''Provides some stats about Nginx logs stored in MongoDB.
-    '''
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    print_nginx_request_logs(client.logs.nginx)
-    print_top_ips(client.logs.nginx)
+''' Get the top 10 most common IPs '''
+top_ips = ip_counter.most_common(10)
 
-
-if __name__ == '__main__':
-    run()
+''' Print the results '''
+print("Top 10 IPs:")
+for ip, count in top_ips:
+    print(f"{ip}: {count}")
